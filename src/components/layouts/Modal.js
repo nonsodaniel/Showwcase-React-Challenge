255bd86301fc6a6d5f5697fe96691bea16b1e123
nonsodaniel <nonsodaniel07@gmail.com>
2020-09-15 23:35:14 +0200

    Added event listener

--- src/components/layouts/Modal.js
+++ src/components/layouts/Modal.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
 import countries from '../../store/jsons/country.json'
 const AddModal = (props) => {
     const [country, setCountry] = useState('')
     const [school, setSchool] = useState('')
     const [degree, setDegree] = useState('')
+    const [grade, setGrade] = useState('')
     const [field, setField] = useState('')
     const [startYear, setStartYear] = useState('')
     const [endYear, setEndYear] = useState('')
     const [description, setDescription] = useState('')
+    const [countryName, setCountryName] = useState('')
+
 
     const closeModal = () => {
         props.closeModal()
@@ -15,10 +19,21 @@ const AddModal = (props) => {
     const fetchCountries = () => {
         setCountry(countries)
     }
+    const handleCountry = (selectedOption) => {
+        setCountryName(selectedOption)
+    };
+    const handlSave = () => {
+        alert()
+    }
 
     useEffect(() => {
         fetchCountries()
     }, [])
+
+    let countryList = [];
+    countries.forEach((value, i) => {
+        countryList.push({ value: value.name, label: value.name });
+    });
 
     return (
         <div className="modal-container">
@@ -32,22 +47,31 @@ const AddModal = (props) => {
                     <img src="" alt="" />
                     <input type="file" name="" id="" />
                 </div>
-                <form className="modal-form">
+                <form className="modal-form" onSubmit={handlSave}>
                     <div className="form-group">
                         <label htmlFor="country__name">Country</label>
                         <div className="input-field input-field-modal">
                             <i className="fas fa-user"></i>
-                            <input type="text" placeholder="Havard university"
-                                onChange={({ target }) => setSchool(target.value)}
+                            <Select
+                                value={countryName}
+                                onChange={handleCountry}
+                                options={countryList}
+                                className="mySelect"
+                                placeholder="Country"
+                                theme={{
+                                    borderRadius: 0,
+                                    spacing: { controlHeight: 55, baseUnit: 5 },
+                                }}
                             />
+
                         </div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="school__name">School</label>
                         <div className="input-field input-field-modal">
                             <i className="fas fa-user"></i>
-                            <input type="text" placeholder="Havard university"
-                                onChange={({ target }) => setSchool(target.value)}
+                            <input type="text" placeholder="Havard university" value={school}
+                                onChange={({ target }) => setSchool(target.value)} required={true}
                             />
                         </div>
                     </div>
@@ -55,7 +79,10 @@ const AddModal = (props) => {
                         <label htmlFor="school__degree">Degree</label>
                         <div className="input-field input-field-modal">
                             <i className="fas fa-user"></i>
-                            <input type="text" placeholder="Bachelor's" />
+                            <input type="text" placeholder="Bachelor's"
+                                onChange={({ target }) => setDegree(target.value)}
+                                required={true}
+                            />
                         </div>
                     </div>
 
@@ -63,7 +90,10 @@ const AddModal = (props) => {
                         <label htmlFor="school__degree">Field of Study</label>
                         <div className="input-field input-field-modal">
                             <i className="fas fa-user"></i>
-                            <input type="text" placeholder="Computer Science" />
+                            <input type="text" placeholder="Computer Science"
+                                onChange={({ target }) => setField(target.value)}
+                                required={true}
+                            />
                         </div>
                     </div>
                     <div className="row">
@@ -71,14 +101,18 @@ const AddModal = (props) => {
                             <label htmlFor="start__year">Start Year</label>
                             <div className="input-field input-field-modal spaced">
                                 <i className="fas fa-user"></i>
-                                <input type="month" id="start__year" placeholder="Your name" />
+                                <input type="month" id="start__year" placeholder="Start Year"
+                                    onChange={(e) => setStartYear(e.target.value)} required={true}
+                                />
                             </div>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="end__year">Start Year</label>
+                            <label htmlFor="end__year">End Year</label>
                             <div className="input-field input-field-modal">
                                 <i className="fas fa-user"></i>
-                                <input type="month" id="end__year" placeholder="Your name" />
+                                <input type="month" id="end__year" placeholder="End year"
+                                    onChange={(e) => setEndYear(e.target.value)} required={true}
+                                />
                             </div>
                         </div>
                     </div>
@@ -86,13 +120,20 @@ const AddModal = (props) => {
                         <label htmlFor="school__grade">Grade</label>
                         <div className="input-field input-field-modal">
                             <i className="fas fa-user"></i>
-                            <input type="text" id="school__grade" placeholder="Your name" />
+                            <input type="text" id="school__grade" value={grade} placeholder="Your grade"
+                                onChange={(e) => setGrade(e.target.value)} required={true}
+                            />
                         </div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="school__description">Description</label>
                         <div className="input-field input-field-modal">
-                            <textarea name="" id="school__description" cols="30" rows="10"></textarea>
+                            <textarea name="" id="school__description" cols="30" rows="10"
+                                placeholder='Description....'
+                                onChange={({ target }) => setDescription(target.value)}
+                                required={true}
+                                required={true}
+                            ></textarea>
                         </div>
                     </div>
                 </form>
