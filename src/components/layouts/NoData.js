55fd0ec2e41054bc4dd6f7ab488e52b3da9a59f0
nonsodaniel <nonsodaniel07@gmail.com>
2020-09-17 20:11:14 +0200

    Fixed danger text

--- src/components/layouts/NoData.js
+++ src/components/layouts/NoData.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NoData = () => {
     return (
         <div>
             <h1 className='text-center font-3'>No data Aailable!</h1>
             <p className='text-center'>
-                Kindly click on the <b><i>Add New Education</i></b>  button above to get started
+                Kindly click on the  <Link>
+                    <b> Add New Education</b>
+                </Link>  button above to get started
              </p>
         </div>
     )
